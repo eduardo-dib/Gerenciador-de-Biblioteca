049bf1fbc408b15fbf115df3573d06459aaf8a4f
eduardo-dib <cardozoedudib@gmail.com>
2024-04-21 19:17:59 -0300

    métodos para busca e adição de livros

--- Gerenciador de Biblioteca/src/App.java
+++ Gerenciador de Biblioteca/src/App.java
@@ -1,5 +1,17 @@
+import java.util.LinkedList;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Biblioteca biblioteca = new Biblioteca("Biblioteca", new LinkedList<>());
+        Livros livro1 = new Livros("Livro 1", "Um cara ai", 2013, 3);
+        Livros livro2 = new Livros("livro2", "Tom Jobim", 2024, 4);
+        System.out.println(livro1);
+        System.out.println(livro2);
+        biblioteca.adicionarLivro(livro1);
+        biblioteca.adicionarLivro(livro2);
+        System.out.println(biblioteca);
+        System.out.println(biblioteca.buscarLivroCodigo(1));
     }
 }
