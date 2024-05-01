8800a2237f4499e3b87ce90ec0fc99dff82f6438
eduardo-dib <cardozoedudib@gmail.com>
2024-05-01 12:43:23 -0300

    reformulação do projeto

--- Gerenciador de Biblioteca/src/App.java
+++ Gerenciador de Biblioteca/src/App.java
@@ -5,17 +5,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        Biblioteca biblioteca = new Biblioteca("Biblioteca", new LinkedList<>());
-        Livros livro1 = new Livros("Livro 1", "Um cara ai", 2013,"Terror", 3);
-        Livros livro2 = new Livros("livro2", "Tom Jobim", 2024,"Samba", 4);
-        Livros livro3 = new Livros("livro3", "Tom Jobim", 2024,"Samba", 4);
-        biblioteca.adicionarLivro(livro1);
-        biblioteca.adicionarLivro(livro2);
-        biblioteca.adicionarLivro(livro3);
-        System.out.println(biblioteca.buscarLivroCategoria("samba "));
-        Usuario usuario1 = new Usuario("Nome do cabra", "41995400686", "Wet Sand", "Alguma coisa ai @gmail", new ArrayList<>());
-        System.out.println(usuario1);
-
 
     }
 }
