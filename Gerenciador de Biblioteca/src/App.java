9ddf0c09746391ac5b2a53a2f389b3405c240bf0
eduardo-dib <cardozoedudib@gmail.com>
2024-05-01 21:39:46 -0300

    duvidas

--- Gerenciador de Biblioteca/src/App.java
+++ Gerenciador de Biblioteca/src/App.java
@@ -2,6 +2,14 @@ import Usuarios.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+
+
+//Ver estrutura do projeto(Herança-Usuario, UsuarioComum e UsuarioPremium, Interface-ILivro, LivroComum e LivroPremium)
+//Como funcionaria a interface Ilivros? sendo que todos os métodos de adicionar, remover livros ficariam na biblioteca
+//Como funcionaria pra classe usuário ter uma lista de livros, seria na classe mãe ou cada classe teria uma lista?
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Gerenciamento de Biblioteca");
